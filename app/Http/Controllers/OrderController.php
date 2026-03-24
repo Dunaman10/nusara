1c29f232b80044eb84cf8f886675c1c3fb136e9f
Syarif Hidayatullah <dunaman1004@gmail.com>
2026-03-24 11:22:57 +0700

    Daftar Pesanan

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
   public function index()
   {
-    $orders = Order::all();
+    $orders = Order::all()->sortByDesc('created_at');
 
     return view('admin.order.index', compact('orders'));
   }
